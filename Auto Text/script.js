49e5ccff3d6e41e77a7b0d34f61d1d87a14369be
Muhammad Khan <muhammadkhan08561@gmail.com>
2025-09-13 22:47:57 +0500

    adding Auto Text Effect project

---
+++ Auto Text/script.js
@@ -0,0 +1,23 @@
+const textElem = document.getElementById('text')
+const speedElem = document.getElementById('speed')
+const text = 'We Love Programming'
+
+
+let idx = 1
+let speed = 300 / speedElem.value
+
+writeText()
+
+function writeText() {
+    textElem.innerHTML = text.slice(0, idx)
+
+    idx++
+
+    if (idx > text.length) {
+        idx = 1
+    }
+
+    setTimeout(writeText, speed)
+}
+
+speedElem.addEventListener('input', (e) => speed = 300 / e.target.value)
